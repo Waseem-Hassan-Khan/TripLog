1073bf6ad1d2f58841cc215628421e0d0af5a780
waseem.h <waseem.h@NOB-IT037>
2024-02-16 23:09:31 +0500

    Add project files.

---
+++ TripLog/Controllers/HomeController.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Web.Mvc;
+using TripLog.Models;
+
+namespace TripLog.Controllers
+{
+    public class HomeController : Controller
+    {
+        private string connString = "Server=(localdb);Database=TripLog;Integrated Security=True;";
+
+        public ActionResult Index()
+        {
+            List<TripLogModel> tripLogs = new List<TripLogModel>();
+
+            using (SqlConnection connection = new SqlConnection(connString))
+            {
+                connection.Open();
+
+                using (SqlCommand command = new SqlCommand("SELECT * FROM TripLogs", connection))
+                {
+                    using (SqlDataReader reader = command.ExecuteReader())
+                    {
+                        while (reader.Read())
+                        {
+                            TripLogModel tripLog = new TripLogModel
+                            {
+                                TripId = (int)reader["TripId"],
+                                Destination = (string)reader["Destination"],
+                                StartDate = (DateTime)reader["StartDate"],
+                                EndDate = (DateTime)reader["EndDate"],
+                                Accommodation = (string)reader["Accommodation"],
+                                AccommodationPhone = (string)reader["AccommodationPhone"],
+                                AccommodationEmail = (string)reader["AccommodationEmail"],
+                                ThingToDo1 = (string)reader["ThingToDo1"],
+                                ThingToDo2 = (string)reader["ThingToDo2"],
+                                ThingToDo3 = (string)reader["ThingToDo3"]
+                            };
+
+                            tripLogs.Add(tripLog);
+                        }
+                    }
+                }
+            }
+
+            return View(tripLogs);
+        }
+    }
+}
---
+++ TripLog/Models/TripLogModel.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace TripLog.Models
+{
+    public class TripLogModel
+    {
+            public int TripId { get; set; }
+            public string Destination { get; set; }
+            public DateTime StartDate { get; set; }
+            public DateTime EndDate { get; set; }
+            public string Accommodation { get; set; }
+            public string AccommodationPhone { get; set; }
+            public string AccommodationEmail { get; set; }
+            public string ThingToDo1 { get; set; }
+            public string ThingToDo2 { get; set; }
+            public string ThingToDo3 { get; set; }
+        }
+    }
+
+
+    /* 
+     * 
+     USE master;
+    GO
+
+    -- Create a new database
+    CREATE DATABASE TripLog;
+    GO
+
+    -- Switch to the TripLog database
+    USE TripLog;
+    GO
+
+    -- Create the TripLog table
+    CREATE TABLE TripLogs
+    (
+        TripId INT PRIMARY KEY IDENTITY(1,1),
+        Destinantion NVARCHAR(255),
+        StartDate DATETIME,
+        EndDate DATETIME,
+        Accommodation NVARCHAR(255),
+        AccommodationPhone NVARCHAR(20),
+        AccommodationEmail NVARCHAR(255),
+        ThingToDo1 NVARCHAR(255),
+        ThingToDo2 NVARCHAR(255),
+        ThingToDo3 NVARCHAR(255)
+    );
+    GO 
+     *
+     */
